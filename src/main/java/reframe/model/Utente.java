4495ef25ef9fccea4164d370a6dfbba8977d622e
39338 <39338@192.168.56.1>
2026-05-31 18:37:27 +0200

    Creazione classe Utente e aggiunta del mysql-connector

--- src/main/java/reframe/model/Utente.java
+++ src/main/java/reframe/model/Utente.java
@@ -1,5 +1,103 @@
 package reframe.model;
 
-public class Utente {
+public class Utente 
+{
+	    private String username;
+	    private String email;
+	    private String password;
+	    private String telefono;
+	    private String nome;
+	    private String cognome;
+	    private String bio;
+	    private String token;
 
-}
+	    // Costruttore
+	    
+	    public Utente(String username, String email, String password, String telefono, String nome, String cognome, String bio, String token) {
+	        this.username = username;
+	        this.email = email;
+	        this.password = password;
+	        this.telefono = telefono;
+	        this.nome = nome;
+	        this.cognome = cognome;
+	        this.bio = bio;
+	        this.token = token;
+	    }
+
+	    // Getter/Setter
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+	    public String getTelefono() {
+	        return telefono;
+	    }
+
+	    public void setTelefono(String telefono) {
+	        this.telefono = telefono;
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public String getCognome() {
+	        return cognome;
+	    }
+
+	    public void setCognome(String cognome) {
+	        this.cognome = cognome;
+	    }
+
+	    public String getBio() {
+	        return bio;
+	    }
+
+	    public void setBio(String bio) {
+	        this.bio = bio;
+	    }
+
+	    public String getToken() {
+	        return token;
+	    }
+
+	    public void setToken(String token) {
+	        this.token = token;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return "Utente{" +
+	                "username='" + username + '\'' +
+	                ", email='" + email + '\'' +
+	                ", nome='" + nome + '\'' +
+	                ", cognome='" + cognome + '\'' +
+	                ", token='" + token + '\'' +
+	                '}';
+	    }
+	}
